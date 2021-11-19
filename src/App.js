80e22905725bb6c0636d530c2ceeb6af011771a8
rosemarym2 <rosemarymay91@gmail.com>
2021-11-19 19:25:54 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,74 @@
 import { useState } from "react";
+import List from "./List";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-import logo from './logo.svg';
-import './App.css';
-import Note from './Note';
-import List from './List';
+const App = () => {
+    const [input, setInput] = useState("");
+    const [ note, setNote] = useState (["Note 1", "Note 2","Note 3"]);
+    
 
+    const addNote = () =>{
+                let storedNote = [...note]
+                storedNote.push(input)
+                setNote(storedNote)
+            }
+    const removeNote = (index) => {
+                let storedNote = [...note]
+                storedNote.splice(index, 1)
+                setNote(storedNote)
+    }        
 
-
-const App =(List) => {
-  return (
-    <div className="App">
-        {List.map((List, index)=>{
+    const changeHandler = (event) => {
+        setInput(event.target.value)
+    }
+    return (
+        <div className ="container">
+            <h1>My Notes:</h1>
+            <input type="text" placeholder="Add note here" onChange={changeHandler}/>
+            <button onClick={addNote}>add note</button>
+            <p>{input}</p>  
+            {note.map((note, index) => {
             return (
-              <div>
-             <Note title = {List.title} content = {List.content}/>
-             <p key ={index}></p>
-             </div>
-            );
+            <div key={index}>                   
+            <h2 className="note">{note}
+            <button className="delete-btn" onClick={() => removeNote (index)}><DeleteForeverIcon /></button></h2>
+            </div> 
+            )
         })}
-            
-    </div>
-     
-  );
+        </div>
+    );
 }
 
-export default App;
+export default App
+
+
+// import { useState } from "react";
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Note from './Note';
+// import List from './List';
+
+
+
+// const App =(List) => {
+//   return (
+//     <div className="App">
+//         {List.map((List, index)=>{
+//             return (
+//               <div>
+//              <Note title = {List.title} content = {List.content}/>
+//              <p key ={index}></p>
+//              </div>
+//             );
+//         })}
+            
+//     </div>
+     
+//   );
+// }
+
+// export default App;
 
 // const App =() => {
 //   return (
